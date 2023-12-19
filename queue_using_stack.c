10ce0f8946c46e3eda43c6dc8c0a6d3265194b2b
Sanya Sharma <sanya6018.be21@chitkara.edu.in>
2023-12-19 12:41:22 +0530

    Implement queue using stack

---
+++ queue_using_stack.c
@@ -0,0 +1,83 @@
+struct Stack{
+    int top;
+    int arr;
+};
+bool isEmpty(struct Stack mystack){
+    return mystack-top == -1;
+}
+struct Stack createStack(int capacity){
+    struct Stack stack = (struct Stack)malloc(sizeof(struct Stack));
+    stack-top = -1;
+    stack-arr = (int)malloc(capacitysizeof(int));
+    return stack;
+}
+void push(struct Stack mystack,int x){
+    mystack-arr[++mystack-top] = x;
+}
+int pop(struct Stack mystack){
+    if(isEmpty(mystack)){
+        return -1;
+    }
+    int temp = mystack-arr[mystack-top--];
+    return temp;
+}
+int peek(struct Stack mystack){
+    if(isEmpty(mystack)){
+        return -1;
+    }
+    int temp = mystack-arr[mystack-top];
+    return temp;
+}
+typedef struct {
+    struct Stack s1;
+    struct Stack s2;
+    
+} MyQueue;
+
+
+MyQueue myQueueCreate() {
+    MyQueue queue = (MyQueue)malloc(sizeof(MyQueue));
+    queue-s1 = createStack(100);
+    queue-s2 = createStack(100);
+    return queue;
+}
+
+void myQueuePush(MyQueue obj, int x) {
+    push(obj-s1,x);
+}
+
+int myQueuePop(MyQueue obj) {
+    if(isEmpty(obj-s1)){
+        return -1;
+    }
+    while(!isEmpty(obj-s1)){
+        push(obj-s2,pop(obj-s1));
+    }
+    int temp = pop(obj-s2);
+    while(!isEmpty(obj-s2)){
+        push(obj-s1,pop(obj-s2));
+    }
+    return temp;
+}
+
+int myQueuePeek(MyQueue obj) {
+    if(isEmpty(obj-s1)){
+        return -1;
+    }
+    while(!isEmpty(obj-s1)){
+        push(obj-s2,pop(obj-s1));
+    }
+    int temp = peek(obj-s2);
+    while(!isEmpty(obj-s2)){
+        push(obj-s1,pop(obj-s2));
+    }
+    return temp;
+}
+
+bool myQueueEmpty(MyQueue obj) {
+    return isEmpty(obj-s1) && isEmpty(obj-s2);
+}
+
+void myQueueFree(MyQueue obj) {
+    free(obj);
+}
